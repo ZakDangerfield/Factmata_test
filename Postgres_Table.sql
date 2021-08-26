01e562138434ad27008deb67e90d5aa63ba943d8
Zak Dangerfield <78031378+ZakDangerfield@users.noreply.github.com>
2021-08-26 20:22:30 +0100

    Update Postgres_Table.sql

--- Postgres_Table.sql
+++ Postgres_Table.sql
@@ -1,6 +1,4 @@
 CREATE TABLE `news` (
-  `id` varchar(255) NOT NULL,
-  `title` varchar(255) NOT NULL,
   'score' varchar(255) NOT NULL,
   `url` varchar(255) NOT NULL,
   `text` varchar(300000) NOT NULL
